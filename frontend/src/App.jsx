1b171da873337e6b1b0352ffd61e7a34d4a697e0
Wilian Zembrani <Wilian3355@gmail.com>
2025-02-05 20:14:10 -0300

    Navbar criada

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
 import Collection from './pages/Collection';
+import About from './pages/About';
 import Contact from './pages/Contact';
+import Product from './pages/Product';
+import Cart from './pages/Cart';
+import Login from './pages/Login';
+import PlaceOrder from './pages/PlaceOrder';
+import Orders from './pages/Orders';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
-      <Router>
+      <Navbar />
+      <Routes>
         <Route path="/" element={<Home />} />
-        <Router path="/Collection" element={<Collection />} />
-        <Router path="/about" element={<About />} />
-        <Router path="/contact" element={<Contact />} />
-        <Router path="/product/:productId" element={<Product />} />
-        <Router path="/cart" element={<Cart />} />
-        <Router path="/login" element={<Login />} />
-        <Router path="/place-order" element={<PlaceOrder />} />
-        <Router path="/orders" element={<Orders />} />
-      </Router>
+        <Route path="/Collection" element={<Collection />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/orders" element={<Orders />} />
+      </Routes>
     </div>
   );
 };
