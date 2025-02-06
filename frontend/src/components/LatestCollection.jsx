48d4bf885b3a253903ea593c8a6087a7e131fbc5
Wilian Zembrani <Wilian3355@gmail.com>
2025-02-06 20:23:25 -0300

    Adicionar listagem de últimas coleções

---
+++ frontend/src/components/LatestCollection.jsx
@@ -0,0 +1,33 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { ShopContext } from '../context/ShopContext';
+import Title from './Title';
+import ProductItem from './ProductItem';
+
+const LatestCollection = () => {
+  const { products } = useContext(ShopContext);
+  const [latestProducts, setLatestProducts] = useState([]);
+
+  useEffect(() => {
+    setLatestProducts(products.slice(0, 10));
+  }, []);
+
+  return (
+    <div className="my-10">
+      <div className="text-center py-8 text-3xl">
+        <Title text1={'ULTIMAS'} text2={'COLEÇÕES'} />
+        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere doloribus quisquam veritatis ducimus possimus
+          voluptatum animi molestias accusantium excepturi quo, laborum dolor est sed, assumenda ullam id sequi? Labore,
+          at.
+        </p>
+      </div>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {latestProducts.map((item, index) => (
+          <ProductItem hey={index} id={item._id} image={item.image} name={item.name} price={item.price} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default LatestCollection;
