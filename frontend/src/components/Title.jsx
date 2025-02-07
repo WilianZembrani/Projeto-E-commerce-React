437716e80a349f04753528c2266939f6d8fa150e
Wilian Zembrani <Wilian3355@gmail.com>
2025-02-06 21:22:23 -0300

    Adiciona a listagem dos produtos mais vendidos

--- frontend/src/components/Title.jsx
+++ frontend/src/components/Title.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Title = ({ text1, text2 }) => {
   return (
-    <div className="inline-flex gap-2 items-cneter mb-3">
-      <p className="text-gray-500">
+    <div className="inline-flex gap-2 items-center mb-3">
+      <p className="flex gap-2 text-gray-500">
         {text1}
         <span className="text-gray-700">{text2}</span>
       </p>
