8b84a480bb660b5815c4fd2c6b5e15bab670c25e
Wilian Zembrani <Wilian3355@gmail.com>
2025-02-10 20:49:41 -0300

    feat: atualiza as políticas da loja

--- frontend/src/components/OurPolicy.jsx
+++ frontend/src/components/OurPolicy.jsx
@@ -9,6 +9,16 @@ const OurPolicy = () => {
         <p className="font-simibold">Política De Troca</p>
         <p className="text-gray-400">Oferecemos uma política de troca sem complicações</p>
       </div>
+      <div>
+        <img src={assets.quality_icon} className="w-12 m-auto mb-5" alt="" />
+        <p className="font-simibold">Devolução em 7 dias</p>
+        <p className="text-gray-400">Nós oferecemos uma política de devolução gratuita de 7 dias.</p>
+      </div>
+      <div>
+        <img src={assets.support_img} className="w-12 m-auto mb-5" alt="" />
+        <p className="font-simibold">O melhor atendimento ao cliente.</p>
+        <p className="text-gray-400">Oferecemos suporte ao cliente 24/7.</p>
+      </div>
     </div>
   );
 };
