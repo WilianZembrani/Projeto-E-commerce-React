5f212afc82e8f027eae985175480288385e29d4f
Wilian Zembrani <Wilian3355@gmail.com>
2025-02-27 14:37:51 -0300

    feat: adiciona componentes NavBar e SideBar com rotas
    - Cria componente ‘NavBar’ com logo e botão de logout
    - Cria componente ‘SideBar’ com links para:
      - Adicionar itens
      - Listar itens
      - Ver ordens
    - Configura rotas para cada link usando

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import NavBar from './components/NavBar';
+import SideBar from './components/SideBar';
+import { Routes, Route } from 'react-router-dom';
+import Add from './pages/Add';
+import List from './pages/List';
+import Orders from './pages/Orders';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="bg-gray-50 min-h-screen ">
+      <>
+        <NavBar />
+        <hr className="border-0.5 border-gray-300" />
+        <div className="flex w-full ">
+          <SideBar />
+          <div className="w-[70%] mx-auto ml-[max(5vw, 25px) my-8 text-gray-600 text-base]">
+            <Routes>
+              <Route path="/add" element={<Add />} />
+              <Route path="/list" element={<List />} />
+              <Route path="/Orders" element={<Orders />} />
+            </Routes>
+          </div>
+        </div>
+      </>
+    </div>
+  );
+};
 
-export default App
+export default App;
