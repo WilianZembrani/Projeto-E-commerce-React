9cc022620b5c4aa573dbed282883e1acb2305065
Wilian Zembrani <Wilian3355@gmail.com>
2025-02-28 00:17:01 -0300

    feat: adiciona page List para listar e remover produtos
    - Exibe imagem, nome, categoria e preço de cada produto
    - Permite remover produtos
    - Integra com backend para buscar e remover produtos via API

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,6 +10,7 @@ import { ToastContainer } from 'react-toastify';
 
 
 export const backendUrl = import.meta.env.VITE_BACKEND_URL;
+export const currency = 'R$'
 
 const App = () => {
   const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : '');
